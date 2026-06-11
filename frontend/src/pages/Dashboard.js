32bcb2bcf45c2952eae50402eeec3c7c9c11b281
qbly224 <bah552359@gmail.com>
2026-06-11 18:25:57 +0200

    Fix Dashboard - useRef chargement initial

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import api from '../services/api';
